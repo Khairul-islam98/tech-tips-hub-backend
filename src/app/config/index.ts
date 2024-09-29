e62f46bb5e7985a115a3575ec5509cd494e3d77d
khairul-islam98 <janeyrahman900@gmail.com>
2024-09-30 00:17:59 +0600

    create config file

---
+++ src/app/config/index.ts
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.join((process.cwd(), '.env')) });
+
+export default {
+  node_env: process.env.NODE_ENV,
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+  jwt_access_secret: process.env.JWT_ACCESS_SECRET,
+  jwt_access_secret_expires_in: process.env.JWT_ACCESS_SECRET_EXPIRE,
+  bcrypt_salt_round: process.env.BCRYPT_SALT_ROUND,
+  reset_pass_ui_link: process.env.RESET_PASS_UI_LINK,
+  email_user: process.env.EMAIL_USER,
+  email_pass: process.env.EMAIL_PASS,
+};
