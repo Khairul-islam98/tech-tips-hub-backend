4a31da6e4cc70164aa22aef6b8fb36d9410c7f29
khairul-islam98 <janeyrahman900@gmail.com>
2024-09-30 00:59:42 +0600

    create user model schema

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+import { Model } from 'mongoose';
+import { USER_ROLE, USER_STATUS } from './user.constant';
+import { Types } from 'mongoose';
+
+export interface IUser {
+  _id?: string;
+  name: string;
+  email: string;
+  password: string;
+  role: keyof typeof USER_ROLE;
+  status: keyof typeof USER_STATUS;
+  profilePhoto?: string;
+  isVerified: boolean;
+  followers: Types.ObjectId[];
+  following: Types.ObjectId[];
+}
+
+export interface IUserModel extends Model<IUser> {
+  isUserExist(email: string): Promise<IUser>;
+  isPasswordMatched(
+    plainTextPassword: string,
+    hashedPassword: string,
+  ): Promise<boolean>;
+}
