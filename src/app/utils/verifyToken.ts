d4d982b4d791ddfedabf002f4c60eeee56bb847e
khairul-islam98 <janeyrahman900@gmail.com>
2024-09-30 00:41:17 +0600

    create utils and routes

---
+++ src/app/utils/verifyToken.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import AppError from '../errors/AppError';
+
+export const verifyToken = (
+  token: string,
+  secret: string,
+): JwtPayload | Error => {
+  try {
+    return jwt.verify(token, secret) as JwtPayload;
+  } catch (err: any) {
+    throw new AppError(401, 'You are not authorized!');
+  }
+};
