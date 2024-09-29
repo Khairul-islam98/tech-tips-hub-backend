d6f91e45557d6e0a1c2abc0c04f4657a8b9fde27
khairul-islam98 <janeyrahman900@gmail.com>
2024-09-30 01:02:56 +0600

    create auth

---
+++ src/app/modules/auth/auth.controller.ts
@@ -0,0 +1,50 @@
+import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import { AuthServices } from './auth.service';
+
+const register = catchAsync(async (req, res) => {
+  const result = await AuthServices.registerUser(req.body);
+  sendResponse(res, {
+    statusCode: 201,
+    success: true,
+    message: 'User registered successfully',
+    data: result,
+  });
+});
+
+const login = catchAsync(async (req, res) => {
+  const result = await AuthServices.loginUser(req.body);
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: 'User logged in successfully',
+    data: result,
+  });
+});
+const forgetPassword = catchAsync(async (req, res) => {
+  const userEmail = req.body.email;
+  const result = await AuthServices.forgetPassword(userEmail);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Reset link is generated succesfully',
+    data: result,
+  });
+});
+const resetPassword = catchAsync(async (req, res) => {
+  const result = await AuthServices.resetPassword(req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Password reset succesful!',
+    data: result,
+  });
+});
+
+export const AuthController = {
+  register,
+  login,
+  forgetPassword,
+  resetPassword,
+};
