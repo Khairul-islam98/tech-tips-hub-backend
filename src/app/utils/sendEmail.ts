f1ed49d033d0e4784199e7ca7941cea2bf4c6e2a
khairul-islam98 <janeyrahman900@gmail.com>
2024-10-25 14:38:23 +0600

    production send mail bug fix

--- src/app/utils/sendEmail.ts
+++ src/app/utils/sendEmail.ts
@@ -5,7 +5,7 @@ export const sendEmail = async (to: string, resetLink: string) => {
   const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 587,
-    secure: config.node_env === 'production',
+    secure: false,
     auth: {
       user: config.email_user,
       pass: config.email_pass,
@@ -41,11 +41,21 @@ export const sendEmail = async (to: string, resetLink: string) => {
     </div>
   `;
 
-  await transporter.sendMail({
+  const mailData = {
     from: `"Tech-tips-hub" <${config.email_user}>`, // sender address
     to, // list of receivers
     subject: 'Reset your password within ten mins', // Subject line
     text: '', // plain text body
     html, // html body
+  };
+
+  await new Promise((resolve, reject) => {
+    transporter.sendMail(mailData, (err, info) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(info);
+      }
+    });
   });
 };
